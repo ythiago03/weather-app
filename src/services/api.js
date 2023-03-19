c5348b2933a24687a99f753ae6c626ce224297ec
Princess Mortix <47502554+princessmortix@users.noreply.github.com>
2023-03-19 14:20:55 -0300

    Fix mixed http content in api.js
    Else you can't make requests under a https connection.

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ const KEY = 'c1253d06c0cf4499a78215642231703'
 
 export const fetchData = async (city) => {
     try {
-        const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}=no`
+        const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}=no`
         const fetchResponses = await fetch(url)
         const data = await fetchResponses.json()
 
